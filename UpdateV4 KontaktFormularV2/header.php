f7ad525e97d5786c303811243402efe9c9c3568c
Carlzon0815 <68272490+Carlzon0815@users.noreply.github.com>
2020-12-06 20:12:36 +0100

    06.12.2020 20:12

---
+++ UpdateV4 KontaktFormularV2/header.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html lang="de" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Imkereibedarf | Franz Simmang | Webseite</title>
+    <link rel="icon" type="image/png" href="ICON.png">
+  </head>
